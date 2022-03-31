c947f2b4e782e6ac2fb12996f8668488f808be77
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-03-31 18:57:33 +0400

    Правки после тестирования

--- source/js/modules/nav/nav.js
+++ source/js/modules/nav/nav.js
@@ -2,22 +2,16 @@ const nav = document.querySelector('.nav');
 const headerNavList = document.querySelector('.nav-list');
 const navButton = document.querySelector('.nav__button');
 const headerInner = document.querySelector('.header__inner');
-const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 
 const navOpen = () => {
   nav.classList.remove('is-open');
 
-  if (mediaQuery.matches) {
-    headerNavList.classList.add('visually-hidden');
-  } else {
-    headerNavList.classList.remove('visually-hidden');
-  }
-
   navButton.addEventListener('click', () => {
     navButton.classList.toggle('nav__button-close');
     navButton.classList.toggle('nav__button');
-    headerNavList.classList.toggle('visually-hidden');
+    headerNavList.classList.toggle('nav-list--closed');
+    headerNavList.classList.toggle('nav-list--open');
     nav.classList.toggle('is-open');
     headerInner.classList.toggle('header__active');
   });
