d6782121ee30e3634df3c8f8a3a9185a2006d3fa
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-04-04 20:11:06 +0400

    исправление каталога

--- source/js/modules/nav/nav.js
+++ source/js/modules/nav/nav.js
@@ -2,10 +2,25 @@ const nav = document.querySelector('.nav');
 const headerNavList = document.querySelector('.nav-list');
 const navButton = document.querySelector('.nav__button');
 const headerInner = document.querySelector('.header__inner');
-
+const headerOverlay = document.querySelector('.header');
 
 const navOpen = () => {
+  headerNavList.classList.add('nav-list--closed');
   nav.classList.remove('is-open');
+
+  document.addEventListener('click', (evt) => {
+    if (navButton.classList.contains('nav__button-close')) {
+      if (!nav.contains(evt.target)) {
+        nav.classList.remove('is-open');
+        headerNavList.classList.toggle('nav-list--open');
+        headerNavList.classList.toggle('nav-list--closed');
+        headerOverlay.classList.remove('header__opasity');
+        headerInner.classList.toggle('header__active');
+        navButton.classList.toggle('nav__button-close');
+        navButton.classList.toggle('nav__button');
+      }
+    }
+  });
 
   navButton.addEventListener('click', () => {
     navButton.classList.toggle('nav__button-close');
@@ -14,6 +29,7 @@ const navOpen = () => {
     headerNavList.classList.toggle('nav-list--open');
     nav.classList.toggle('is-open');
     headerInner.classList.toggle('header__active');
+    headerOverlay.classList.toggle('header__opasity');
   });
 };
 
